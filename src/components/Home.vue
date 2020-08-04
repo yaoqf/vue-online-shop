47362f37b189f74831f4e7c368a9eb64e40fd01e
yao <yqf2014@163.com>
2020-08-04 17:00:16 +0800

    添加home页面

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="title"></div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data: function () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
